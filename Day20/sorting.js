c9a6a58e36d5436c4565d1309486a798b5f008f7
manthan ankolekar <manthan.ank46@gmail.com>
2022-05-27 21:49:57 +0530

    day20

--- Day20/sorting.js
+++ Day20/sorting.js
@@ -1,7 +1,32 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
 function main() {
-    var n = parseInt(readLine());
-    a = readLine().split(' ');
-    a = a.map(Number);
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
     // Write Your Code Here
     var swapped;
     var numSwaps = 0;

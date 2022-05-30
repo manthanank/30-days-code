1aae74a22b493901a99d8d0f494c90bc122fc417
manthan ankolekar <manthan.ank46@gmail.com>
2022-05-30 10:49:29 +0530

    day23

---
+++ Day23/bts_level_order_traversel.js
@@ -0,0 +1,87 @@
+// Start of function Node
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}; // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    // Start of function levelOrder
+    this.levelOrder = function(root) {
+
+        // Add your code here
+        // To print values separated by spaces use process.stdout.write(someValue + ' ')
+
+        //start que with root node
+        var que = [root];
+
+        //if que has length at all
+        while( que.length ) 
+        {
+            //shift removes first element in array and returns it.
+            var treeNode = que.shift();
+           
+            //print the node's data that was just removed
+            process.stdout.write(treeNode.data + ' ');
+           
+            //if node has a left child, push to que
+            if(treeNode.left) 
+            {
+                que.push(treeNode.left);
+            }    
+           
+            //if node has right child, push to que
+            if(treeNode.right) 
+            {
+                que.push(treeNode.right);
+            }
+        }
+	}; // End of function levelOrder
+}; // End of function BinarySearchTree
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _input = "";
+
+process.stdin.on('data', function (data) {
+    _input += data;
+});
+
+process.stdin.on('end', function () {
+    var tree = new BinarySearchTree();
+    var root = null;
+    
+    var values = _input.split('\n').map(Number);
+    
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    tree.levelOrder(root);
+});

a3a8001c04c28ed8cc6c0d53e5a6234069283c60
manthan ankolekar <manthan.ank46@gmail.com>
2022-06-03 09:56:44 +0530

    day27

---
+++ Day27/testing.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    console.log("OK");
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+
+
+function main() {
+    const t: number = parseInt(readLine().trim(), 10);
+
+    for (let tItr: number = 0; tItr < t; tItr++) {
+        const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n: number = parseInt(firstMultipleInput[0], 10);
+
+        const k: number = parseInt(firstMultipleInput[1], 10);
+
+        const a: number[] = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+    }
+}
